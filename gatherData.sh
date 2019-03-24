3c0bc62a02a6090a7a19be52aa0518190290e48f
Sam Wehunt <sam.wehunt@gmail.com>
2019-03-24 16:32:41 -0500

    added fancy script to collect data while a test is running

---
+++ gatherData.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#time in seconds
+test_time=3600
+
+start_time=`date +%s`
+curr_time=`date +%s`
+
+while [ $(($curr_time-$start_time)) -lt $test_time ]
+do
+    docker stats --no-stream | tail -n +2 | tr -s ' ' | cut -d ' ' -f 1,2,3,4 | tr ' ' ',' | \
+    while read i
+    do
+        echo $curr_time","$i
+    done
+    sleep 1
+    curr_time=`date +%s`
+
+done
